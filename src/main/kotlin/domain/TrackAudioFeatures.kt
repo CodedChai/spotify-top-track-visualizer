7d369fb0e3d3edbc3ccf6ffe9a28c225e02a62ac
codedchai <guardianx515@gmail.com>
2025-12-01 23:19:33 -0600

    Add track audio features

---
+++ src/main/kotlin/domain/TrackAudioFeatures.kt
@@ -0,0 +1,14 @@
+package domain
+
+data class TrackAudioFeatures(
+  val acousticness: Float,
+  val danceability: Float,
+  val energy: Float,
+  val durationMs: Int,
+  val liveness: Int,
+  val valence: Int,
+  val trackHref: String,
+  val tempo: Float,
+  val uri: String,
+  val id: String,
+)
